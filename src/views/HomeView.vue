603bd42da3e92a7d19413d419bfb0280c432fdba
Cory Dowdy <cdowdy46@gmail.com>
2023-01-02 14:33:15 -0500

    add some initial grid css

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,16 @@
 
 <template>
   <main>
-
+    <Formkit type="form" >
+      <FormKit type="radio"
+               name="preferedMeasure"
+               id="preferedMeasure"
+               label="Use Metric or US Customary"
+               :options="{
+                    metric: 'Metric',
+                    customary: 'US Customary',
+              }"
+      />
+    </Formkit>
   </main>
 </template>
