4029ae482ef5e3b2bc79047b29bbe52192a7be76
Cory Dowdy <cdowdy46@gmail.com>
2023-01-03 10:56:09 -0500

    update app.vue for initial ionic

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-</script>
-
 <template>
-
-  <RouterView />
+  <ion-app>
+    <ion-router-outlet/>
+  </ion-app>
 </template>
 
-<style scoped>
+<script>
+import { IonApp, IonRouterOutlet } from '@ionic/vue';
+import { defineComponent } from 'vue';
 
-</style>
+export default defineComponent({
+  name: 'App',
+  components: {
+    IonApp,
+    IonRouterOutlet,
+  },
+});
+</script>
