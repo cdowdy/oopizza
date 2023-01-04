c527818d815eaebd3173d3d10bc83f4894dd4d3b
Cory Dowdy <cdowdy46@gmail.com>
2023-01-04 11:50:04 -0500

    remove pico in favor of tailwind i guess :(

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
-import '@picocss/pico/css/pico.min.css'
+// import '@picocss/pico/css/pico.min.css'
 
 const app = createApp(App)
 
