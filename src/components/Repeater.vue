19a765ebd018d600db0b45ac7d60ef0ee76113e9
Cory Dowdy <cdowdy46@gmail.com>
2023-01-25 11:03:26 -0500

    initial repeater component

---
+++ src/components/Repeater.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+// set the props this component will receive
+const props = defineProps({
+  context: Object
+})
+
+function handleInput(e) {
+  props.context.node.input(e.target.value)
+}
+
+</script>
+
+<template>
+  <input @input="handleInput" :value="props.context._value" />
+</template>
